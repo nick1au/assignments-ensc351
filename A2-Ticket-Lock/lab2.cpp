171fe1f20c4ff72c95be0bcf526dba5afe9aedc7
Nicholas Lau <nick.l.ttn@gmail.com>
2018-10-10 20:29:57 -0400

    Working method2 (spinlock) on std::thread

--- A2-Ticket-Lock/lab2.cpp
+++ A2-Ticket-Lock/lab2.cpp
@@ -29,10 +29,10 @@ void *print_message_function( void *ptr );
 //void *start_func(void *ptr);
 void start_func(int ix);
 void create_threads(int num);
-void add_one_brute();
+void add_one();
 
 bool all_threads_created = false;
-const int num_threads = 3;
+const int num_threads = 100;
 //bool pthread_lock = false;
 
 // pthread_imp
@@ -42,10 +42,11 @@ const int num_threads = 3;
 std::thread arr_th[num_threads];
 std::mutex thread_lock;
 
-const int method = 1; // 0 = brute-force, 1 = spin, 2 = ticket queue
+const int method = 2; // 1 = brute-force, 2 = spin, 3 = ticket queue
 int count = 0;
 
-std::atomic_flag flag[num_threads] = {ATOMIC_FLAG_INIT};
+//std::atomic_flag flag[num_threads] = {ATOMIC_FLAG_INIT};
+bool flag[num_threads] = {true}; // false = locked, true = available
 
 using namespace std;
 using namespace std::chrono;
@@ -57,10 +58,11 @@ int main(){
         //pthread_join(arr_th[i], NULL); // pthread_imp
         arr_th[i].join();
         if (i == 0){
-            flag[i].clear(std::memory_order_acquire);
+            //flag[i].clear(std::memory_order_acquire);
+            flag[0] = true;
         }
         else{
-            flag[i].test_and_set(std::memory_order_acquire);
+            //flag[i].test_and_set(std::memory_order_acquire);
         }
     } // similar to join()
     printf("DONE processing '\n'");
@@ -86,39 +88,37 @@ void start_func(int ix){
     //cout << "IX THREAD " << ix << endl;
     start = system_clock::now();
     //printf("RELEASED '\n'");
-    if (method == 0){ // brute-force
+    if (method == 1){ // brute-force
         
         //pthread_mutex_lock(&pthread_lock); // pthread_imp
         thread_lock.lock();
-        add_one_brute();
+        add_one();
         //pthread_mutex_unlock(&pthread_lock); // pthread_imp
         thread_lock.unlock();
     }
-    else if (method == 1){ // spin-lock
+    else if (method == 2){ // spin-lock
         //...
-        while(flag[ix].test_and_set(std::memory_order_acquire)){}
+        while(flag[ix] == false){}//flag[ix].test_and_set(std::memory_order_acquire)){}
         //cout << "IX THREAD " << ix << endl;
-        printf("IX_THREAD %d '\n' ", ix);
-        //if (ix > 0){
-         //   arr_th[ix-1].join();
-        //}
-        flag[ix].clear(std::memory_order_acquire);
+        // printf("IX_THREAD %d '\n' ", ix);
+        //flag[ix].clear(std::memory_order_acquire);
         if (ix < num_threads){
-            flag[ix+1].clear(std::memory_order_acquire);
+            //flag[ix+1].clear(std::memory_order_acquire);
+            flag[ix + 1] = true;
         }
     }
-    else if (method == 2){ //ticket-lock
+    else if (method == 3){ //ticket-lock
         
     }
 
     end = system_clock::now();
 	duration<double> elapsed_seconds = end - start;
-    //cout << std::fixed << std::setprecision(15) << elapsed_seconds.count() <<endl;
+    cout << std::fixed << std::setprecision(15) << elapsed_seconds.count() <<endl;
 	//cout << std::setprecision(15)
     //cout << elapsed_seconds.count() <<endl;
 }
 
-void add_one_brute(){  
+void add_one(){  
     count++;
     //printf("count = %d '\n'", count);
 }
