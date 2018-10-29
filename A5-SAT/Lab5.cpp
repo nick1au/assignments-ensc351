f20591da8df9ed8b6f5d70f26db0f2cc18dbf359
Nicholas Lau <nick.l.ttn@gmail.com>
2018-10-29 16:53:45 -0700

    Initial Project folder for Lab 5

---
+++ A5-SAT/Lab5.cpp
@@ -0,0 +1,14 @@
+#include <fstream>
+#include <iostream>
+#include <vector>
+#include <thread>
+#include <functional>
+#include <string>
+#include <mutex>
+#include <chrono>
+#include <ctime>
+#include <stdlib.h>
+#include <algorithm>
+#include <iterator>
+#include <omp.h>
+
